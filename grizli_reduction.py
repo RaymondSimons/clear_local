8f6d30fd05e21974840433fc59f49815f96fb025
RaymondSimons <rsimons@jhu.edu>
2018-09-11 14:34:43 -0400

    testing

--- grizli_reduction.py
+++ grizli_reduction.py
@@ -139,8 +139,8 @@ def grizli_model(visits, field = 'GN2', ref_filter_1 = 'F105W', ref_grism_1 = 'G
             if (ref_filter_1.lower() in filt1) or (ref_filter_2.lower() in filt1):
                 #Find grism files with a direct image
                 all_direct_files.extend(visit['files'])
-                grism_index_1= np.where((basenames == basename) & (ref_grism_1.lower() in filter_names))
-                grism_index_2= np.where((basenames == basename) & (ref_grism_2.lower() in filter_names))
+                grism_index_1= np.where((basenames == basename) & (ref_grism_1.lower() in filter_names))[0]
+                grism_index_2= np.where((basenames == basename) & (ref_grism_2.lower() in filter_names))[0]
 
                 if len(grism_index_1) > 0:
                     all_grism_files.extend(visits[grism_index_1[0]]['files'])
