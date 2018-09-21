7ae280f0db19e0cb5baaaccf50d926fc44738864
RaymondSimons <rsimons@jhu.edu>
2018-09-20 20:52:48 -0400

    default testing message

--- refit_grizli_withphot.py
+++ refit_grizli_withphot.py
@@ -15,5 +15,28 @@ os.chdir(PATH_TO_PREP)
 eazy.symlink_eazy_inputs(path=os.path.dirname(eazy.__file__)+'/data', 
                          path_is_env=False)
 
+field = 'goodsn'
+version = 'v4.1'
+
+params = {}
+params['CATALOG_FILE'] = '{0}_3dhst.{1}.cats/Catalog/{0}_3dhst.{1}.cat'.format(field, version)
+params['Z_STEP'] = 0.002
+params['Z_MAX'] = 4
+
+params['MAIN_OUTPUT_FILE'] = '{0}_3dhst.{1}.eazypy'.format(field, version)
+params['PRIOR_FILTER'] = 205
+
+# Galactic extinction
+params['MW_EBV'] = {'aegis':0.0066, 'cosmos':0.0148, 'goodss':0.0069, 
+                    'uds':0.0195, 'goodsn':0.0103}[field]
+
+params['TEMPLATES_FILE'] = 'templates/fsps_full/tweak_fsps_QSF_12_v3.param'
+
+translate_file = '{0}_3dhst.{1}.cats/Eazy/{0}_3dhst.{1}.translate'.format(field, version)
+
+ez = eazy.photoz.PhotoZ(param_file=None, translate_file=translate_file, 
+                        zeropoint_file=None, params=params, 
+                        load_prior=True, load_products=False)
+
 
 os.chdir(PATH_TO_SCRIPTS)
