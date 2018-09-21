9a4b046ea9332ce2739ec62eb448728e8854e7ea
RaymondSimons <rsimons@jhu.edu>
2018-09-20 20:47:51 -0400

    default testing message

--- refit_grizli_withphot.py
+++ refit_grizli_withphot.py
@@ -1,2 +1,18 @@
 import grizli
 import eazy
+import os
+
+PATH_TO_RAW = '/user/rsimons/grizli_extractions/RAW'
+PATH_TO_PREP = '/user/rsimons/grizli_extractions/PREP'
+PATH_TO_SCRIPTS = '/home/rsimons/git/clear_local'
+PATH_TO_CATS= '/user/rsimons/grizli_extractions/Catalogs'
+
+
+
+
+os.chdir(PATH_TO_PREP)
+
+
+
+
+os.chdir(PATH_TO_SCRIPTS)
