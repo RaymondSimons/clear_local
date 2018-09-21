094a685539c4e4fbc1b9776c0a398d030021151a
RaymondSimons <rsimons@jhu.edu>
2018-09-21 01:05:24 -0400

    default testing message

--- grizli_reduction.py
+++ grizli_reduction.py
@@ -542,8 +542,8 @@ if __name__ == '__main__':
     PATH_TO_CATS= '/user/rsimons/grizli_extractions/Catalogs'
 
     os.chdir(PATH_TO_PREP)
-    mag_lim = 21
-    mag_lim_lower = 0
+    mag_lim = 22
+    mag_lim_lower = 21
 
     id_choose = 23116
     if True:
