4f8c5eb0810c89bfa60e51e84dafc9c640856963
RaymondSimons <rsimons@jhu.edu>
2018-12-05 16:25:56 -0500

    default testing message

--- grizli_reduction.py
+++ grizli_reduction.py
@@ -19,6 +19,7 @@ import eazy
 from joblib import Parallel, delayed
 from glob import glob
 from mastquery import query, overlaps
+matplotlib.use('agg')
 '''
 try: 
     from mastquery import query, overlaps
