9208a535658e5a08770e9a570333324a23e73b19
RaymondSimons <rsimons@jhu.edu>
2018-12-09 16:47:49 -0500

    default testing message

--- write_line_catalog.py
+++ write_line_catalog.py
@@ -15,7 +15,7 @@ for field in ['GS1']:#, 'GS2', 'GS3', 'GS5', 'GN1', 'GN2', 'GN3', 'GN4', 'GN5', 
     fits_name = '/user/rsimons/grizli_extractions/Catalogs/%s_lines_grizli.fits'%field
 
 
-    lines = ['OII', 'OIII', 'Ha', 'Hb']
+    lines = ['OII', 'OIII', 'Ha', 'Hb', 'SII', 'SIII']
 
 
     cat.write('#(0) ID\n')
@@ -30,13 +30,17 @@ for field in ['GS1']:#, 'GS2', 'GS3', 'GS5', 'GN1', 'GN2', 'GN3', 'GN4', 'GN5', 
     cat.write('#(9) Ha flux err, 1e-17 erg/s/cm2\n')
     cat.write('#(10) Hb flux, 1e-17 erg/s/cm2\n')
     cat.write('#(11) Hb flux err, 1e-17 erg/s/cm2\n')
-    cat.write('#(12) z_50\n')
-    cat.write('#(13) z_02\n')
-    cat.write('#(14) z_16\n')
-    cat.write('#(15) z_84\n')
-    cat.write('#(16) z_97\n')
+    cat.write('#(12) SII flux, 1e-17 erg/s/cm2\n')
+    cat.write('#(13) SII flux err, 1e-17 erg/s/cm2\n')
+    cat.write('#(14) SIII flux, 1e-17 erg/s/cm2\n')
+    cat.write('#(15) SIII flux err, 1e-17 erg/s/cm2\n')
+    cat.write('#(16) z_50\n')
+    cat.write('#(17) z_02\n')
+    cat.write('#(18) z_16\n')
+    cat.write('#(19) z_84\n')
+    cat.write('#(20) z_97\n')
 
-    fluxs = zeros((4,2, len(fls))) - 99.
+    fluxs = zeros((len(lines),2, len(fls))) - 99.
     zs = zeros((5, len(fls))) - 99.
     IDs = []
     ras = []
@@ -67,10 +71,12 @@ for field in ['GS1']:#, 'GS2', 'GS3', 'GS5', 'GN1', 'GN2', 'GN3', 'GN4', 'GN5', 
                 flux_ln =  a[0].header['FLUX%.3i'%(l+1)]
                 eflux_ln =  a[0].header['ERR%.3i'%(l+1)]
                 ln_name =  a[0].header['LINE%.3i'%(l+1)]			
-                if ln_name == 'OII': 		j = 0
-                elif ln_name == 'OIII':  	j = 1
-                elif ln_name == 'Ha': 		j = 2
-                elif ln_name == 'Hb': 		j = 3
+                if ln_name == lines[0]: 		j = 0
+                elif ln_name == lines[1]:  	j = 1
+                elif ln_name == lines[2]: 		j = 2
+                elif ln_name == lines[3]: 		j = 3
+                elif ln_name == lines[4]:       j = 4
+                elif ln_name == lines[5]:       j = 5
 
                 if j != -99:
                     fluxs[j,0,f] = flux_ln * 1.e17
@@ -79,7 +85,7 @@ for field in ['GS1']:#, 'GS2', 'GS3', 'GS5', 'GN1', 'GN2', 'GN3', 'GN4', 'GN5', 
 
 
         cat.write('%i\t\t%.8f\t\t%.8f\t\t%i\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\n'%
-            (IDs[f],  ras[f], decs[f], nlines[f], fluxs[0,0,f], fluxs[0,1,f], fluxs[1,0,f], fluxs[1,1,f], fluxs[2,0,f], fluxs[2,1,f], fluxs[3,0,f], fluxs[3,1,f], zs[0, f], zs[1, f], zs[2, f], zs[3, f], zs[4, f]))
+            (IDs[f],  ras[f], decs[f], nlines[f], fluxs[0,0,f], fluxs[0,1,f], fluxs[1,0,f], fluxs[1,1,f], fluxs[2,0,f], fluxs[2,1,f], fluxs[3,0,f], fluxs[3,1,f], fluxs[4,0,f], fluxs[4,1,f],fluxs[5,0,f], fluxs[5,1,f], zs[0, f], zs[1, f], zs[2, f], zs[3, f], zs[4, f]))
 
     master_hdulist = []
     prihdr = fits.Header()
@@ -101,6 +107,10 @@ for field in ['GS1']:#, 'GS2', 'GS3', 'GS5', 'GN1', 'GN2', 'GN3', 'GN4', 'GN5', 
     fits.Column(name='Ha_e',format = 'D', array=fluxs[2,1,:]),
     fits.Column(name='Hb_f',format = 'D', array=fluxs[3,0,:]),
     fits.Column(name='Hb_e',format = 'D', array=fluxs[3,1,:]),
+    fits.Column(name='SII_f',format = 'D', array=fluxs[4,0,:]),
+    fits.Column(name='SII_e',format = 'D', array=fluxs[4,1,:]),
+    fits.Column(name='SIII_f',format = 'D', array=fluxs[5,0,:]),
+    fits.Column(name='SIII_e',format = 'D', array=fluxs[5,1,:]),
     fits.Column(name='z_50',format = 'D', array=zs[0,:]),
     fits.Column(name='z_02',format = 'D', array=zs[1,:]),
     fits.Column(name='z_16',format = 'D', array=zs[2,:]),
