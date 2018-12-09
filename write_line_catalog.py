3f71f336ae3d9c84b4caadf345c284a83d3c75e6
RaymondSimons <rsimons@jhu.edu>
2018-12-09 16:27:38 -0500

    default testing message

--- write_line_catalog.py
+++ write_line_catalog.py
@@ -7,78 +7,74 @@ from glob import glob
 for field in ['GS1']:#, 'GS2', 'GS3', 'GS5', 'GN1', 'GN2', 'GN3', 'GN4', 'GN5', 'GN7']:
 
 
-	if True:
-		fls = glob('/user/rsimons/grizli_extractions/PREP/*.full.fits')
-		cat = open('/user/rsimons/grizli_extractions/Catalogs/%s_lines_grizli.cat'%field, 'w+')
-	else:
-		fls = glob('/Volumes/wd/clear/%s/*/PREP/*.full.fits'%field)
-		cat = open('/Users/rsimons/Desktop/clear/Catalogs/%s_lines_grizli.cat'%field, 'w+')
+
+    fls = glob('/user/rsimons/grizli_extractions/PREP/*.full.fits')
+    cat = open('/user/rsimons/grizli_extractions/Catalogs/%s_lines_grizli.cat'%field, 'w+')
 
 
 
-
-	lines = ['OII', 'OIII', 'Ha', 'Hb']
-
-
-	cat.write('#(0) ID\n')
-	cat.write('#(1) ra\n')
-	cat.write('#(2) dec\n')
-	cat.write('#(3) n_lines\n')
-	cat.write('#(4) OII flux, 1e-17 erg/s/cm2\n')
-	cat.write('#(5) OII flux err, 1e-17 erg/s/cm2\n')
-	cat.write('#(6) OIII flux, 1e-17 erg/s/cm2\n')
-	cat.write('#(7) OIII flux err, 1e-17 erg/s/cm2\n')
-	cat.write('#(8) Ha flux, 1e-17 erg/s/cm2\n')
-	cat.write('#(9) Ha flux err, 1e-17 erg/s/cm2\n')
-	cat.write('#(10) Hb flux, 1e-17 erg/s/cm2\n')
-	cat.write('#(11) Hb flux err, 1e-17 erg/s/cm2\n')
-	cat.write('#(12) z_50\n')
-	cat.write('#(13) z_02\n')
-	cat.write('#(14) z_16\n')
-	cat.write('#(15) z_84\n')
-	cat.write('#(16) z_97\n')
-
-	fluxs = zeros((4,2, len(fls))) - 99.
-	zs = zeros((5, len(fls))) - 99.
-	IDs = []
-	ras = []
-	decs = []
-	n_lines = []
-	for f, fl in enumerate(fls):
-		a = fits.open(fl)
-		IDs[f] = a[0].header['ID']
+    lines = ['OII', 'OIII', 'Ha', 'Hb']
 
 
-		ras[f] = a[0].header['ra']
-		decs[f] = a[0].header['dec']
-		lines_f = a[0].header['HASLINES'].split(' ')
-		nlines[f] = a[0].header['NUMLINES']
-		if (len(lines_f) > 0) & (lines_f[0] !=''):
-			j = -99
+    cat.write('#(0) ID\n')
+    cat.write('#(1) ra\n')
+    cat.write('#(2) dec\n')
+    cat.write('#(3) n_lines\n')
+    cat.write('#(4) OII flux, 1e-17 erg/s/cm2\n')
+    cat.write('#(5) OII flux err, 1e-17 erg/s/cm2\n')
+    cat.write('#(6) OIII flux, 1e-17 erg/s/cm2\n')
+    cat.write('#(7) OIII flux err, 1e-17 erg/s/cm2\n')
+    cat.write('#(8) Ha flux, 1e-17 erg/s/cm2\n')
+    cat.write('#(9) Ha flux err, 1e-17 erg/s/cm2\n')
+    cat.write('#(10) Hb flux, 1e-17 erg/s/cm2\n')
+    cat.write('#(11) Hb flux err, 1e-17 erg/s/cm2\n')
+    cat.write('#(12) z_50\n')
+    cat.write('#(13) z_02\n')
+    cat.write('#(14) z_16\n')
+    cat.write('#(15) z_84\n')
+    cat.write('#(16) z_97\n')
 
-			zs[0, f] = a[1].header['Z50']
-			zs[1, f] = a[1].header['Z02']
-			zs[2, f] = a[1].header['Z16']
-			zs[3, f] = a[1].header['Z84']
-			zs[4, f] = a[1].header['Z97']
-
-			for l, ln in enumerate(lines_f):
-				flux_ln =  a[0].header['FLUX%.3i'%(l+1)]
-				eflux_ln =  a[0].header['ERR%.3i'%(l+1)]
-				ln_name =  a[0].header['LINE%.3i'%(l+1)]			
-				if ln_name == 'OII': 		j = 0
-				elif ln_name == 'OIII':  	j = 1
-				elif ln_name == 'Ha': 		j = 2
-				elif ln_name == 'Hb': 		j = 3
-
-				if j != -99:
-					fluxs[j,0,f] = flux_ln * 1.e17
-					fluxs[j,1,f] = eflux_ln  * 1.e17
-				j = -99
+    fluxs = zeros((4,2, len(fls))) - 99.
+    zs = zeros((5, len(fls))) - 99.
+    IDs = []
+    ras = []
+    decs = []
+    n_lines = []
+    for f, fl in enumerate(fls):
+        a = fits.open(fl)
+        IDs[f] = a[0].header['ID']
 
 
-		cat.write('%i\t\t%i\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.8f\t\t%.8f\t\t%i\n'%
-				  (IDs[f],  ras[f], decs[f], nlines[f], fluxs[0,0,f], fluxs[0,1,f], fluxs[1,0,f], fluxs[1,1,f], fluxs[2,0,f], fluxs[2,1,f], fluxs[3,0,f], fluxs[3,1,f], zs[0, f], zs[1, f], zs[2, f], zs[3, f], zs[4, f]))
+        ras[f] = a[0].header['ra']
+        decs[f] = a[0].header['dec']
+        lines_f = a[0].header['HASLINES'].split(' ')
+        nlines[f] = a[0].header['NUMLINES']
+        if (len(lines_f) > 0) & (lines_f[0] !=''):
+            j = -99
+
+            zs[0, f] = a[1].header['Z50']
+            zs[1, f] = a[1].header['Z02']
+            zs[2, f] = a[1].header['Z16']
+            zs[3, f] = a[1].header['Z84']
+            zs[4, f] = a[1].header['Z97']
+
+            for l, ln in enumerate(lines_f):
+                flux_ln =  a[0].header['FLUX%.3i'%(l+1)]
+                eflux_ln =  a[0].header['ERR%.3i'%(l+1)]
+                ln_name =  a[0].header['LINE%.3i'%(l+1)]			
+                if ln_name == 'OII': 		j = 0
+                elif ln_name == 'OIII':  	j = 1
+                elif ln_name == 'Ha': 		j = 2
+                elif ln_name == 'Hb': 		j = 3
+
+                if j != -99:
+                    fluxs[j,0,f] = flux_ln * 1.e17
+                    fluxs[j,1,f] = eflux_ln  * 1.e17
+                j = -99
+
+
+        cat.write('%i\t\t%i\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.8f\t\t%.8f\t\t%i\n'%
+            (IDs[f],  ras[f], decs[f], nlines[f], fluxs[0,0,f], fluxs[0,1,f], fluxs[1,0,f], fluxs[1,1,f], fluxs[2,0,f], fluxs[2,1,f], fluxs[3,0,f], fluxs[3,1,f], zs[0, f], zs[1, f], zs[2, f], zs[3, f], zs[4, f]))
 
 
 
@@ -93,8 +89,8 @@ for field in ['GS1']:#, 'GS2', 'GS3', 'GS5', 'GN1', 'GN2', 'GN3', 'GN4', 'GN5', 
     col1 = fits.Column(name='ID', array=IDs)
     col2 = fits.Column(name='RA', array=ras)
     col3 = fits.Column(name='DEc', array=decs)
-	coldefs = fits.ColDefs([col1, col2, col3])
-	table_hdu = fits.BinTableHDU.from_columns(coldefs)
+    coldefs = fits.ColDefs([col1, col2, col3])
+    table_hdu = fits.BinTableHDU.from_columns(coldefs)
     master_hdulist.append(table_hdu)
 
 
