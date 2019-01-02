1f38508fc87a5d34732e9aa40f882bc3bbf8a043
RaymondSimons <rsimons@jhu.edu>
2019-01-02 14:28:35 -0500

    default testing message

--- stack_maps.py
+++ stack_maps.py
@@ -9,15 +9,36 @@ if __name__ == '__main__':
     prep_dir = '/Volumes/gdrive/clear/grizli_extractions/*/*/Prep'
     full_fls = glob(prep_dir + '/*full.fits')
     if False:
-        all_full = []
+        all_full_Ha = []
+        all_full_OII = []
+        all_full_OIII = []
+        all_full_Hb = []
+
         for f, full_fl in enumerate(full_fls):
             print f, len(full_fls)
             full = fits.open(full_fl)
+            '''
             if 'Ha' in full[0].header['HASLINES']:
-                all_full.append(full['LINE', 'Ha'].data)
+                all_full_Ha.append(full['LINE', 'Ha'].data)
+
+            if 'Hb' in full[0].header['HASLINES']:
+                all_full_Hb.append(full['LINE', 'Hb'].data)
+            '''
+            if 'OII ' in full[0].header['HASLINES']:
+                all_full_OII.append(full['LINE', 'OII'].data)
+            elif "OII'" in full[0].header['HASLINES']:
+                all_full_OII.append(full['LINE', 'OII'].data)
+
+            if 'OIII ' in full[0].header['HASLINES']:
+                all_full_OIII.append(full['LINE', 'OIII'].data)
+            elif "OIII'" in full[0].header['HASLINES']:
+                all_full_OIII.append(full['LINE', 'OIII'].data)
+
+            full.close()
+
     if True:
-        all_full = array(all_full)
-        all_full[all_full == 0.] = nan
-        ha_median = nanmedian(all_full, axis = 0)
+        #all_full_OIII = array(all_full_OIII)
+        #all_full_OIII[all_full_OIII == 0.] = nan
+        #ha_median = nanmedian(all_full_OIII, axis = 0)
         clf()
         imshow(ha_median, interpolation = 'nearest', cmap = 'Greys_r')
