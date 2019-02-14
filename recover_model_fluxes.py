317cdac581c9e44e2157adf20544af64a6775b2d
RaymondSimons <rsimons@jhu.edu>
2019-02-14 11:24:18 -0500

    default testing message

--- recover_model_fluxes.py
+++ recover_model_fluxes.py
@@ -1,6 +1,8 @@
 import astropy
 from astropy.io import fits
 import grizli
+from grizli import utils
+
 from grizli.pipeline import auto_script
 from grizli.multifit import GroupFLT, MultiBeam, get_redshift_fit_defaults
 import glob
@@ -20,6 +22,20 @@ out_dir = '/user/rsimons/grizli_extractions/Catalogs/bestfit_model_fluxes/%s/'%f
 for fl in fls[0:1]:
     out_file = out_dir + fl.split('/')[-1].replace('full.fits', 'fluxes.cat')
     data = fits.open(fl)
+
+    di = int(fl.split('/')[-1].split('_').strip('full.fits'))
+    mb = MultiBeam('{0}_{1:05d}.beams.fits'.format(field, di), group_name=field)
+
+    BOUNDED_DEFAULTS = {'method':'bvls', 'tol':1.e-8, 'verbose':0}
+    templ1 = grizli.utils.load_templates(fwhm=1200, line_complexes=False, stars=False, 
+                                     full_line_list=None, continuum_list=None, 
+                                     fsps_templates=True)
+
+    tfit = mb.template_at_z(z = data[1].header['Z_MAP'], templates = templ1, fit_background=True, fitter='nnls', bounded_kwargs=BOUNDED_DEFAULTS)
+
+    A_phot = mb._interpolate_photometry(z=tfit['z'], templates=t1)
+    A_model = A_phot.T.dot(data[1].data['coeffs'])
+
     print (out_file)
 
 
