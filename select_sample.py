596f15f3fd3248c2a4f761144a089ebd08b3f973
RaymondSimons <rsimons@jhu.edu>
2019-03-07 10:16:22 -0500

    default testing message

--- select_sample.py
+++ select_sample.py
@@ -7,7 +7,7 @@ from glob import glob
 cat_fls = glob('/Users/rsimons/Desktop/clear/Catalogs/grizli_v2.1_cats/*_lines_grizli.fits')
 
 f = open('/Users/rsimons/Dropbox/rcs_clear/z_r_sample.cat', 'w+')
-
+tot = 0
 for c, cat_fl in enumerate(cat_fls):
     cat = fits.open(cat_fl)
     O2_f = cat[1].data['OII_FLUX']
@@ -16,12 +16,13 @@ for c, cat_fl in enumerate(cat_fls):
     O3_ef = cat[1].data['OIII_FLUX_ERR']
     good = where((O2_f/O2_ef > 5.) & ((O3_f/O3_ef > 5.)))[0]
     print len(good)
+    tot+=len(good)
     field = cat_fl.split('/')[-1].strip('lines_grizli.fits')
     print field
     for g, gd in enumerate(good):
         f.write('%s\t%.5i\n'%(field, cat[1].data['ID'][gd]))
 
-
+print tot
 f.close()
 
 
