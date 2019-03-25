c3d6de1cd9954bd0867b6c85b62aa91fab6838f8
RaymondSimons <rsimons@jhu.edu>
2019-03-25 13:40:24 -0400

    default testing message

---
+++ mv_full_samples.py
@@ -0,0 +1,25 @@
+import os
+import glob
+
+
+
+line_sets = [R23,
+             R2,
+             R3,
+             S3,
+             O32,
+             O3S2,
+             HaHb]
+
+
+
+sample_fulls_dir = '/user/rsimons/grizli_extractions/sample_fulls'
+
+
+
+cat_dir = '/user/rsimons/grizli_extractions/Catalogs'
+for line in line_sets:
+    cat = np.loadtxt(cat_dir + '/sample_cats/%s_sample.cat'%line, dtype = 'str')
+    for c in cat:
+        fl = glob('/user/rsimons/grizli_extractions/%s/*/Prep/%s_%s.full.fits'%(c[0], c[0], c[1]))
+        print fl
