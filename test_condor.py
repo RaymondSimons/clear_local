4a221a8aad00807bcf82aa451f337e50c4309ef0
RaymondSimons <rsimons@jhu.edu>
2019-04-01 16:22:42 -0400

    default testing message

--- test_condor.py
+++ test_condor.py
@@ -1,23 +1,2 @@
 #!/usr/bin/env python
-from sys import argv
-import time
-import math
-import numpy as np
-values = None
-try:
-   values = [ int(i) for i in argv[1:] ]
-except:
-   exit(1)
-
-
-
-for i in np.arange(1.e5):
-    x = math.sqrt(i)
-
-time.sleep(100)
-
-
-
-
-print sum(values)
-
+which python
