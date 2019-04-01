c44388bc1caed24a52c3e7af417d1f52a5d895f2
RaymondSimons <rsimons@jhu.edu>
2019-04-01 15:10:40 -0400

    default testing message

--- test_condor.py
+++ test_condor.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 from sys import argv
-
+import time
 values = None
 try:
    values = [ int(i) for i in argv[1:] ]
 except:
    exit(1)
+
+
+time.sleep(40)
+
 
 print sum(values)
 
