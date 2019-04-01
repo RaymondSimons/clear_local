594a4a36cf020c7133545c3562272591ed12e259
RaymondSimons <rsimons@jhu.edu>
2019-04-01 17:39:29 -0400

    default testing message

--- test_condor.py
+++ test_condor.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 import os
-os.sytem('export $PATH="${PATH};/home/rsimons/miniconda2/envs/grizli/bin"')
+os.system('export $PATH="${PATH};/home/rsimons/miniconda2/envs/grizli/bin"')
 os.system('echo $PATH')
