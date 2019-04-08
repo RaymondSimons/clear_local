bc2a8e2c76b4668c4a142209b99d58e78506b362
RaymondSimons <rsimons@jhu.edu>
2019-04-08 17:36:09 -0400

    default testing message

--- metal_maps_mcmc.py
+++ metal_maps_mcmc.py
@@ -246,24 +246,24 @@ if __name__ == '__main__':
     print (b-a)
     '''
     #sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(R, Rerr, diagnostics))
+    with Pool() as pool:
+        a = time.time()
+        sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(R, Rerr, diagnostics))
+        sampler.run_mcmc(pos, 10000)       
+        b = time.time()
+        print (b-a)
 
-    a = time.time()
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(R, Rerr, diagnostics))
-    sampler.run_mcmc(pos, 10000)       
-    b = time.time()
-    print (b-a)
+    with Pool() as pool:
+        a = time.time()
+        sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob_data_glob)
+        sampler.run_mcmc(pos, 10000)       
+        b = time.time()
+        print (b-a)
 
+    #samples = sampler.chain[:, 300:, :].reshape((-1, ndim))
 
-    a = time.time()
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob_data_glob)
-    sampler.run_mcmc(pos, 10000)       
-    b = time.time()
-    print (b-a)
-
-    samples = sampler.chain[:, 300:, :].reshape((-1, ndim))
-
-    OH_mcmc = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]), zip(*np.percentile(samples, [16, 50, 84], axis=0)))    
-    print (list(OH_mcmc))
+    #OH_mcmc = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]), zip(*np.percentile(samples, [16, 50, 84], axis=0)))    
+    #print (list(OH_mcmc))
 
 
 
