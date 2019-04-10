a8eacf89cd40fa769c7ac90dc1c209666501d273
RaymondSimons <rsimons@jhu.edu>
2019-04-10 17:36:00 -0400

    default testing message

--- metal_maps_mcmc.py
+++ metal_maps_mcmc.py
@@ -373,7 +373,7 @@ if __name__ == '__main__':
 
             for i in arange(shape(Rs)[1]):
                 for j in arange(shape(Rs)[2]):
-                    print (i.j)
+                    print (i,j)
                     Rs_ij = array([Rs[d][i,j]])
                     eRs_ij = array([eRs[d][i,j]])
 
