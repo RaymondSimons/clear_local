5cfcc5525327f70835e68fdc250f8ddfcbcf4d8c
RaymondSimons <rsimons@jhu.edu>
2019-06-24 16:36:10 -0400

    default testing message

--- metallicity_profiles.py
+++ metallicity_profiles.py
@@ -5,11 +5,22 @@ import glob
 from glob import glob
 import numpy as np
 from numpy import *
+import photutils
+from photutils import detect_sources
 
 
 def make_metal_profile(fl):
     zfits = fits.open(fl)
-    zmap = zfits['Z_ALL'].data
+    try: zmap = zfits['Z_R3'].data
+    except: return
+    imR = zfits['R3'].data
+    eimR = zfits['eR3'].data
+    imR[xmn_pix:xmx_pix, xmn_pix:xmx_pix] = nan
+    eimR[xmn_pix:xmx_pix, xmn_pix:xmx_pix] = nan
+    imR[(imR<0) | (eimR/imR/log(10) > 0.4)] = nan
+
+
+    segm = detect_sources(imR, -99, npixels=5)
 
     x1 = 40.
     y1 = 40.
@@ -19,6 +30,12 @@ def make_metal_profile(fl):
 
     xv, yv = np.meshgrid(x, y)
     r = sqrt(xv**2. + yv**2.)
+
+
+
+
+
+
 
 if __name__ == '__main__':
 
