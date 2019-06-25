41142a530c539c7ff9c4eb4349085960c89093a4
RaymondSimons <rsimons@jhu.edu>
2019-06-24 21:10:06 -0400

    default testing message

--- reduce_metal_fits.py
+++ reduce_metal_fits.py
@@ -4,12 +4,15 @@ import os
 import glob
 from glob import glob
 from numpy import *
+from joblib import Parallel, delayed
 os.system('mkdir /user/rsimons/metal_maps_reduced')
 
 
 fls = glob('/user/rsimons/metal_maps/*')
 
-for f, fl in enumerate(fls):
+
+
+def make_new_fits(fl):
     out_file = fl.replace('/metal_maps/', '/metal_maps_reduced/')
     a = fits.open(fl)
     new_hdulist = []
@@ -19,3 +22,7 @@ for f, fl in enumerate(fls):
     thdulist = fits.HDUList(new_hdulist)
     print ('Saving to {}'.format(out_file))
     thdulist.writeto(out_file, overwrite = True)
+    return
+
+
+Parallel(n_jobs = -1)(delayed(make_new_fits) (fl) for f, fl in enumerate(fls))
