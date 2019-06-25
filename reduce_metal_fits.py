12cc7c1e630afde0025e6ff1b61ed45468158247
RaymondSimons <rsimons@jhu.edu>
2019-06-24 21:08:38 -0400

    default testing message

--- reduce_metal_fits.py
+++ reduce_metal_fits.py
@@ -10,7 +10,7 @@ os.system('mkdir /user/rsimons/metal_maps_reduced')
 fls = glob('/user/rsimons/metal_maps/*')
 
 for f, fl in enumerate(fls):
-    out_file = fl.replace('/metal_maps/', 'metal_maps_reduced')
+    out_file = fl.replace('/metal_maps/', '/metal_maps_reduced/')
     a = fits.open(fl)
     new_hdulist = []
     for i in arange(len(a)):
