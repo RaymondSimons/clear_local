53802a14448c21b369a2df71dcd295bd6102b30e
RaymondSimons <rsimons@jhu.edu>
2019-07-17 11:48:01 -0400

    default testing message

--- run_izi.py
+++ run_izi.py
@@ -8,6 +8,15 @@ Vlam = 5470. # from Johnson Cousins_V
 calkV = calk(Vlam)
 tlam = np.array([3727., 5007.,  4863.])
 
+lines = np.array(['oii3726;oii3729','oiii4959;oiii5007',  'hbeta'])
+fluxes = np.array([0.4e-17, 0.8e-17, 1.e-17])
+errors = np.array([0.1e-17, 0.2e-17, 1.e-18])
+
+savfile = 'test.sav'
+
+res = izi(fluxes, errors, lines, idl=idl, dosave=True, savfile=savfile,
+              grid=os.environ['IZI_DIR']+'/grids/d13_kappa20.fits')
+
 # take AV from nZ:
 '''
 Av = nZ['Av'].iloc[i]
