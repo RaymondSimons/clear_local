66b6e90dea0d7b7ab790d277ac11c1eee5b829ca
RaymondSimons <rsimons@jhu.edu>
2020-04-04 20:06:34 -0400

    default testing message

--- grizli_reduction.py
+++ grizli_reduction.py
@@ -426,34 +426,34 @@ def grizli_fit(id, min_id, mag, field = '', mag_lim = 35, mag_lim_lower = 35, ru
                     phot, ii, dd = ep.get_phot_dict(tab['ra'][0], tab['dec'][0])
 
                 # Gabe suggests use_psf = True for point sources
-                #try:
-                out = grizli.fitting.run_all(
-                    id, 
-                    t0=templ0, 
-                    t1=templ1, 
-                    fwhm=1200, 
-                    zr=zr,              #zr=[0.0, 12.0],    #suggests zr = [0, 12.0] if we want to extend redshift fit
-                    dz=[0.004, 0.0005], 
-                    fitter='nnls',
-                    group_name=field + '_new',# + '_%i'%phot_scale_order,
-                    fit_stacks=False,          #suggests fit_stacks = False, fit to FLT files
-                    prior=None, 
-                    fcontam=fcontam,           #suggests fcontam = 0.2
-                    pline=pline, 
-                    mask_sn_limit=np.inf,      #suggests mask_sn_limit = np.inf
-                    fit_only_beams=True,       #suggests fit_only_beams = True
-                    fit_beams=False,           #suggests fit_beams = False
-                    root=field,
-                    fit_trace_shift=False,  
-                    bad_pa_threshold = np.inf, #suggests bad_pa_threshold = np.inf
-                    phot=phot, 
-                    verbose=True, 
-                    scale_photometry=phot_scale_order, 
-                    show_beams=True,
-                    use_psf = use_psf)          #default: False
+                try:
+                    out = grizli.fitting.run_all(
+                        id, 
+                        t0=templ0, 
+                        t1=templ1, 
+                        fwhm=1200, 
+                        zr=zr,              #zr=[0.0, 12.0],    #suggests zr = [0, 12.0] if we want to extend redshift fit
+                        dz=[0.004, 0.0005], 
+                        fitter='nnls',
+                        group_name=field + '_new',# + '_%i'%phot_scale_order,
+                        fit_stacks=False,          #suggests fit_stacks = False, fit to FLT files
+                        prior=None, 
+                        fcontam=fcontam,           #suggests fcontam = 0.2
+                        pline=pline, 
+                        mask_sn_limit=np.inf,      #suggests mask_sn_limit = np.inf
+                        fit_only_beams=True,       #suggests fit_only_beams = True
+                        fit_beams=False,           #suggests fit_beams = False
+                        root=field,
+                        fit_trace_shift=False,  
+                        bad_pa_threshold = np.inf, #suggests bad_pa_threshold = np.inf
+                        phot=phot, 
+                        verbose=True, 
+                        scale_photometry=phot_scale_order, 
+                        show_beams=True,
+                        use_psf = use_psf)          #default: False
 
-                #except:
-                #    pass
+                except:
+                    pass
 
             print('Finished', id, mag)
         else: return
