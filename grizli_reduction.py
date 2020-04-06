80c7b08ff178f06dee5ab20d56a13e7470cabda1
RaymondSimons <rsimons@jhu.edu>
2020-04-06 13:26:07 -0400

    default testing message

--- grizli_reduction.py
+++ grizli_reduction.py
@@ -427,32 +427,34 @@ def grizli_fit(id, min_id, mag, field = '', mag_lim = 35, mag_lim_lower = 35, ru
                     phot, ii, dd = ep.get_phot_dict(tab['ra'][0], tab['dec'][0])
 
                 # Gabe suggests use_psf = True for point sources
-
-                out = grizli.fitting.run_all(
-                    id, 
-                    t0=templ0, 
-                    t1=templ1, 
-                    fwhm=1200, 
-                    zr=zr,              #zr=[0.0, 12.0],    #suggests zr = [0, 12.0] if we want to extend redshift fit
-                    dz=[0.004, 0.0005], 
-                    fitter='nnls',
-                    group_name=field,# + '_%i'%phot_scale_order,
-                    fit_stacks=False,          #suggests fit_stacks = False, fit to FLT files
-                    prior=None, 
-                    fcontam=fcontam,           #suggests fcontam = 0.2
-                    pline=pline, 
-                    mask_sn_limit=np.inf,      #suggests mask_sn_limit = np.inf
-                    fit_only_beams=True,       #suggests fit_only_beams = True
-                    fit_beams=False,           #suggests fit_beams = False
-                    root=field,
-                    fit_trace_shift=False,  
-                    bad_pa_threshold = np.inf, #suggests bad_pa_threshold = np.inf
-                    phot=phot, 
-                    verbose=True, 
-                    scale_photometry=phot_scale_order, 
-                    show_beams=True,
-                    use_psf = use_psf)          #default: False
-
+                try:
+                    out = grizli.fitting.run_all(
+                        id, 
+                        t0=templ0, 
+                        t1=templ1, 
+                        fwhm=1200, 
+                        zr=zr,              #zr=[0.0, 12.0],    #suggests zr = [0, 12.0] if we want to extend redshift fit
+                        dz=[0.004, 0.0005], 
+                        fitter='nnls',
+                        group_name=field,# + '_%i'%phot_scale_order,
+                        fit_stacks=False,          #suggests fit_stacks = False, fit to FLT files
+                        prior=None, 
+                        fcontam=fcontam,           #suggests fcontam = 0.2
+                        pline=pline, 
+                        mask_sn_limit=np.inf,      #suggests mask_sn_limit = np.inf
+                        fit_only_beams=True,       #suggests fit_only_beams = True
+                        fit_beams=False,           #suggests fit_beams = False
+                        root=field,
+                        fit_trace_shift=False,  
+                        bad_pa_threshold = np.inf, #suggests bad_pa_threshold = np.inf
+                        phot=phot, 
+                        verbose=True, 
+                        scale_photometry=phot_scale_order, 
+                        show_beams=True,
+                        use_psf = use_psf)          #default: False
+                except:
+                    print ('EXCEPTION IN FIT', id, mag)
+                    pass
 
             print('Finished', id, mag)
         else: return
@@ -631,7 +633,7 @@ if __name__ == '__main__':
         nums = cat_[0]
         mags = cat_[1]
 
-        '''
+
         Parallel(n_jobs = n_jobs, backend = 'threading')(delayed(grizli_fit)(id = id, min_id = fit_min_id, mag = mag, field = field, 
                                                                              mag_lim = mag_lim, mag_lim_lower = mag_max, run = fit_bool, 
                                                                              id_choose = id_choose, use_pz_prior = False, use_phot = True, 
@@ -639,6 +641,7 @@ if __name__ == '__main__':
                                                                              ep = ep, pline = pline, phot_scale_order = phot_scale_order, use_psf = use_psf, fit_without_phot = fit_without_phot,
                                                                              zr = [args['zr_min'], args['zr_max']]) 
                                                                              for id, mag in zip(nums.astype('int'), mags))
+
         '''
         for id, mag in zip(nums.astype('int'), mags):
             grizli_fit(id = id, min_id = fit_min_id, mag = mag, field = field, 
@@ -647,7 +650,7 @@ if __name__ == '__main__':
                        scale_phot = True, templ0 = templ0, templ1 = templ1, 
                        ep = ep, pline = pline, phot_scale_order = phot_scale_order, use_psf = use_psf, fit_without_phot = fit_without_phot,
                        zr = [args['zr_min'], args['zr_max']])
-
+        '''
 
 
     print ('Changing to %s'%PATH_TO_SCRIPTS)
