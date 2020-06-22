2ebe97f99fadf4aa6ca9f8ed5c93561a7633b412
RaymondSimons <rsimons@jhu.edu>
2020-06-22 14:39:04 -0400

    default testing message

--- grizli_reduction.py
+++ grizli_reduction.py
@@ -381,13 +381,11 @@ def grizli_fit(id, min_id, mag, field = '', mag_lim = 35, mag_lim_lower = 35, ru
                scale_phot = True, templ0 = None, templ1 = None, ep = None, pline = None, 
                fcontam = 0.2, phot_scale_order = 1, use_psf = False, fit_without_phot = True, zr = [0., 12.]):
     
-    if os.path.exists(field + '_' + '%.5i.full.fits'%id): return
+    #if os.path.exists(field + '_' + '%.5i.full.fits'%id): return
 
     if (mag <= mag_lim) & (mag >=mag_lim_lower) & (id > min_id):
-        print (id, id_choose, id == id_choose)
         if (id_choose is not None) & (id != id_choose):  return
         #if os.path.isfile(field + '_' + '%.5i.stack.fits'%id): return
-        print (id)
 
         if os.path.isfile(field + '_' + '%.5i.beams.fits'%id):
             print('Reading in beams.fits file for %.5i'%id)
